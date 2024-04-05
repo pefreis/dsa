3060346e61fcbb908e88b53e3d60f127fe316d77
pefreis <pefreis@gmail.com>
2024-04-04 22:30:47 -0300

    LTC-2520: add c++ solution

---
+++ leetcode/2520_count_the_digits_that_divide_a_number/2520.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countDigits(int num)
+    {
+        int count = 0;
+        int n = num;
+        while (n > 0)
+        {
+            int d = n % 10;
+            if (num % d == 0)
+                count++;
+            n /= 10;
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    int num;
+    cin >> num;
+    Solution sol = Solution();
+    cout << sol.countDigits(num) << endl;
+    return 0;
+}
