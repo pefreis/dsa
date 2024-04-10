d20a30adb5a23596e9fc1e5f190ee9a0f3adddee
pefreis <pefreis@gmail.com>
2024-04-09 21:36:32 -0300

    LTC-0026: add c++ solution

---
+++ leetcode/0026_remove_duplicates_from_sorted_array/0026.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int k = 0;
+        int i = 0;
+        while (i < nums.size())
+        {
+            nums[k] = nums[i];
+            k++;
+
+            int j = i;
+            while (j < nums.size() && nums[j] == nums[i])
+                j++;
+
+            i = j;
+        }
+
+        return k;
+    }
+};
+
+int main()
+{
+    string line;
+    getline(cin, line);
+
+    istringstream iss(line);
+    int num;
+    vector<int> nums;
+    while (iss >> num)
+        nums.push_back(num);
+
+    int res = Solution().removeDuplicates(nums);
+    cout << res << endl;
+    for (auto n : nums)
+        cout << n << " ";
+    cout << endl;
+    return 0;
+}
