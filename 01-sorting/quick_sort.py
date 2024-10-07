bec87ce2c2f77d434cb55866bac8424102713ff7
pefreis <pefreis@gmail.com>
2024-10-07 08:01:00 -0300

    add quick sort implementation with lomuto partitioning

--- 01-sorting/quick_sort.py
+++ 01-sorting/quick_sort.py
@@ -1,3 +1,40 @@
+from numpy.random import randint
+
+def _pick_pivot(arr: list, start_idx: int, end_idx: int) -> int:
+    return randint(start_idx, end_idx+1)
+
+def _lomuto_partition(arr: list, start_idx: int, end_idx: int, pivot_idx: int) -> tuple[int, list]:
+    pivot = arr[pivot_idx]
+    arr[pivot_idx], arr[end_idx] = arr[end_idx], arr[pivot_idx]
+    
+    i = start_idx - 1
+    for j in range(start_idx, end_idx):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    i += 1
+    arr[i], arr[end_idx] = arr[end_idx], arr[i]
+    return i, arr
+
+def _partition(arr: list, start_idx: int, end_idx: int, pivot_idx: int) -> tuple[int, list]:
+    return _lomuto_partition(arr, start_idx, end_idx, pivot_idx)
+
+def _sort(arr: list, start_idx: int, end_idx: int) -> list:
+    if start_idx >= end_idx:
+        return arr
+    
+    p = _pick_pivot(arr, start_idx, end_idx)
+    p, arr = _partition(arr, start_idx, end_idx, p)
+
+    arr = _sort(arr, start_idx, p-1)
+    arr = _sort(arr, p+1, end_idx)
+
+    return arr
+
+def quick_sort(arr: list) -> list:
+    return _sort(arr, 0, len(arr)-1)
+
 def basic_quick_sort(arr: list) -> list:
     if len(arr) <= 1:
         return arr
